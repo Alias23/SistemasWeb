7f32c49d2fbc06c72da99a9e33f411055abc4d99
amartinl89 <andermartin88@gmail.com>
2023-10-05 17:26:49 +0200

    funciona web

--- database.php
+++ database.php
@@ -1,12 +1,13 @@
 <?php 
 
-$host = "127.0.0.1";
+$host = "db";
 $database = "agendacontactos";
+$puerto = "3306";
 $usuario = "root";
 $password = "root";
 
 try{
-  $conn = new PDO("mysql:host=$host;port=8889;dbname=$database", $usuario, $password);
+  $conn = new PDO("mysql:host=$host;port=$puerto;dbname=$database", $usuario, $password);
   // prueba para ver si funciona correctamente:
   // foreach($conn -> query("SHOW DATABASES") as $row){
   //   print_r($row);
