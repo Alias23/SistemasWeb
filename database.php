65fdfe0ee2616c2dd79bf0bc2d0ade80f5ba6bfe
amartinl89 <andermartin88@gmail.com>
2023-10-05 12:58:00 +0200

    Arreglado el driver

--- database.php
+++ database.php
@@ -1,12 +1,12 @@
 <?php 
 
-$host = "localhost";
+$host = "127.0.0.1";
 $database = "agendacontactos";
 $usuario = "root";
-$password = "";
+$password = "root";
 
 try{
-  $conn = new PDO("mysql:host=$host;dbname=$database", $usuario, $password);
+  $conn = new PDO("mysql:host=$host;port=8889;dbname=$database", $usuario, $password);
   // prueba para ver si funciona correctamente:
   // foreach($conn -> query("SHOW DATABASES") as $row){
   //   print_r($row);
