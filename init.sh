d3a8c261f194604bae084d6c76e32c253c115fb0
amartinl89 <andermartin88@gmail.com>
2023-10-13 18:46:35 +0200

    BD automatica

--- init.sh
+++ init.sh
@@ -2,7 +2,9 @@ sudo docker build -t="agendacontactos" .
 openssl genrsa -out llave.key 2048
 openssl req -new -key llave.key -out servidor.csr
 openssl x509 -req -days 365 -in servidor.csr -signkey llave.key -out certificado.crt
+echo Creando base de datos...
+sudo mysql -u root -e "CREATE DATABASE IF NOT EXISTS agendacontactos;"
 echo Importando base de datos...
 sudo mysql -u root -p agendacontactos < ./sql/setup.sql
-sudo docker-compose up
-firefox "http://localhost:81/index.php"
+echo Importación terminada
+docker-compose up -d
