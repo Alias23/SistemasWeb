cc4cdd4a2df02901eea31e3289f96e26e2dbf1f7
amartinl89 <113670011+amartinl89@users.noreply.github.com>
2023-10-19 12:00:00 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,12 @@
-sudo docker build -t="agendacontactos" .
+docker build -t="agendacontactos" ./Docker/Web
 openssl genrsa -out llave.key 2048
 openssl req -new -key llave.key -out servidor.csr
 openssl x509 -req -days 365 -in servidor.csr -signkey llave.key -out certificado.crt
 echo Creando base de datos...
-sudo mysql -u root -e "CREATE DATABASE IF NOT EXISTS agendacontactos;"
+echo Introduce la contraseña root
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS agendacontactos;"
 echo Importando base de datos...
-sudo mysql -u root -p agendacontactos < ./sql/setup.sql
+echo Introduce la contraseña root
+mysql -u root -p agendacontactos < ./sql/setup.sql
 echo Importación terminada
 docker-compose up -d
